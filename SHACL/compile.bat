c5deca7f09e531fabeeb2e9f5511edbaf20a2c2e
liviorobaldo <40432737+liviorobaldo@users.noreply.github.com>
2023-02-12 01:59:14 +0000

    Delete SHACL directory

--- SHACL/compile.bat
+++
@@ -1,1 +0,0 @@
-javac -cp .;./lib/* DetectViolationsOnCorpus.java
--- SHACL/run.bat
+++
@@ -1,3 +0,0 @@
-del ..\DatasetCreation\CORPUS\evaluationSHACL.txt
-java -Xms40G -cp .;./build/classes;./lib/* -Dfile.encoding=utf-8 DetectViolationsOnCorpus "licenceusecaseTBox.owl" "INDEXED_RULES" "compliancerules.ttl" "../DatasetCreation/CORPUS/SHACL"
-REM exit
